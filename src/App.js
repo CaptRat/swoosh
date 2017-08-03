692826fb654bcd701e88e74f1749a99e403b9517
captrat <jpatricklogan@gmail.comgit config user.name>
2017-08-03 13:49:27 -0600

    WIP: seek window size for proper # of squares

--- src/App.js
+++ src/App.js
@@ -5,7 +5,26 @@ import './App.css';
 
 class App extends Component {
   buildSquares() {
-    let anotherarr = new Array(10).fill().map((c, i) => <div className="square">Hello</div>)
+    constructor(props) {
+      super(props);
+      this.state = { width: '0', height: '0' };
+      this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+    }
+
+    componentDidMount() {
+      this.updateWindowDimensions();
+      window.addEventListener('resize', this.updateWindowDimensions);
+    }
+
+    componentWillUnmount() {
+      window.removeEventListener('resize', this.updateWindowDimensions);
+    }
+
+    updateWindowDimensions() {
+      this.setState({ width: window.innerWidth, height: window.innerHeight });
+    }
+
+    let anotherarr = new Array(10000).fill().map((c, i) => <div key={i} className="square"></div>)
     return anotherarr
   }
 
